935701ce7ddb2c2a0fd76a808fbd0c98bc0065a5
Chris Continanza <christopher.continanza@gmail.com>
2013-05-16 19:48:09 -0700

    not even using specs

--- test/test_spec.rb
+++ test/test_spec.rb
@@ -1,13 +1,5 @@
 require 'helper'
 
-class TestSpec < Vault::TestCase
-
-  def test_default_spec_class_is_vault_spec
-    assert_equal Vault::Spec, MiniTest::Spec.spec_type('')
-  end
-
-end
-
 class SpecTest < Vault::Spec
   describe "Vault::Spec" do
     it 'should be runnable' do
