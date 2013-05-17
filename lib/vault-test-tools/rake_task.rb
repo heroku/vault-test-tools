4992b2f21fd35252c812402e702f5d1a2761d2f5
Chris Continanza <christopher.continanza@gmail.com>
2013-05-16 19:48:35 -0700

    do all the magic in the rake task

--- lib/vault-test-tools/rake_task.rb
+++ lib/vault-test-tools/rake_task.rb
@@ -1,8 +1,10 @@
 require 'rake/testtask'
 
+ENV['CPUPROFILE'] = './test/.test.profile'
 Rake::TestTask.new do |t|
   t.verbose = true
   t.ruby_opts << '-r turn/autorun'
+  t.ruby_opts << '-r perftools'
   t.ruby_opts << '-I test'
   t.test_files = FileList['test/**/*_test.rb', 'test/**/*_spec.rb']
 end
