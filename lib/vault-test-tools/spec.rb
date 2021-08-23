3c53790e422f68e7da9a8ef8ee17c5a225a52d83
Benjamin Denton <bdenton@heroku.com>
2021-08-23 09:36:30 -0400

    fix flaky test/code

--- lib/vault-test-tools/spec.rb
+++ lib/vault-test-tools/spec.rb
@@ -4,6 +4,10 @@ require 'scrolls'
 # Base class for Vault spec tests.
 class Vault::Spec < MiniTest::Spec
   before do
+    Scrolls.init(
+      global_context: {app: 'test_app'},
+      time_unit: 'milliseconds'
+    )
     Scrolls.stream = StringIO.new if defined? Scrolls
   end
 end
